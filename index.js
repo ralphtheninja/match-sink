7139c9222c24ad00313fbc847b64fc8b695781f5
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-09-10 04:06:25 +0200

    var -> const/let :sparkles:

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var through = require('through2')
-var util = require('core-util-is')
+const through = require('through2')
+const util = require('core-util-is')
 
 function matchSink (opts, iter) {
-  var regex = util.isRegExp(opts) ? opts : opts.regex
+  const regex = util.isRegExp(opts) ? opts : opts.regex
   if (!util.isRegExp(regex)) throw new Error('missing regular expression')
   if (typeof iter !== 'function') throw new Error('missing callback')
 
-  var matched = false
+  let matched = false
 
   return through(function (chunk, enc, callback) {
-    var str = chunk.toString()
-    var match = regex.exec(str)
+    const match = regex.exec(chunk.toString())
 
     if (match) {
       if (opts.matchAll === true) {
